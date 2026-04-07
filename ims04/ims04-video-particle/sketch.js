f968d888e0eb32787fa5f900c31b2a4791d352ae
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2026-04-07 11:32:13 -0400

    img_color_xy

--- ims04/ims04-video-particle/sketch.js
+++ ims04/ims04-video-particle/sketch.js
@@ -10,7 +10,7 @@
 
 let particles = [];
 
-let res = 16;
+let res = 12;
 
 let img;
 
@@ -35,28 +35,25 @@ function setup() {
 
 function draw() {
   background(255);
-
   for (let i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].draw();
   }
-
   ball.move();
   ball.draw();
   // image(img, 0, 0, width, height);
 }
 
+// init particles arrary
 function placeParticles() {
-  for (let i = 0; i < width; i += res) {
-    for (let j = 0; j < height; j += res) {
-      let x = (i / width) * img.width;
-      let y = (j / height) * img.height;
-      let c = img.get(x, y);
-
-      // if(c[3] != 0) {
-      if (c[0] + c[1] + c[2] != 255 * 3) {
-        particles.push(new Particle(i, j, c));
-      }
+  for (let x = 0; x < width; x += res) {
+    for (let y = 0; y < height; y += res) {
+      // Pickup color from image
+      let c = img_color_xy(x, y);
+      // Non-white pixel gets added -- disabled
+      // if (c[0] + c[1] + c[2] != 255 * 3) {
+      particles.push(new Particle(x, y, c));
+      // }
     }
   }
 }
@@ -65,36 +62,27 @@ class Particle {
   constructor(x, y, c) {
     this.x = x;
     this.y = y;
-
     this.c = c;
-
     this.homeX = x;
     this.homeY = y;
   }
-
   update() {
     // mouse
     let ballDistance = dist(this.x, this.y, ball.x, ball.y);
     let ballAngle = atan2(this.y - ball.y, this.x - ball.x);
-
     // home
     let homeDistance = dist(this.x, this.y, this.homeX, this.homeY);
     let homeAngle = atan2(this.homeY - this.y, this.homeX - this.x);
-
     // forces
     let ballRepulsion = constrain(map(ballDistance, 0, 100, 10, 0), 0, 10);
     let homeAttraction = map(homeDistance, 0, 100, 0, 10);
-
     let vx = cos(ballAngle) * ballRepulsion;
     vx += cos(homeAngle) * homeAttraction;
-
     let vy = sin(ballAngle) * ballRepulsion;
     vy += sin(homeAngle) * homeAttraction;
-
     this.x += vx;
     this.y += vy;
   }
-
   draw() {
     // fill(0, 40);
     // stroke(0, 40);
@@ -110,26 +98,34 @@ class Ball {
   constructor(x_, y_, name_) {
     this.x = x_ || random(width);
     this.y = y_ || random(height);
-    this.radius = 10;
+    this.radius = res;
     this.speedX = 4;
     this.speedY = 2;
     this.color = '#DD0000';
   }
-
   move() {
     //
     this.x += this.speedX;
     this.y += this.speedY;
-
     if (this.x > width) this.speedX = -this.speedX;
     if (this.x < 0) this.speedX = -this.speedX;
-
     if (this.y > height) this.speedY = -this.speedY;
     if (this.y < 0) this.speedY = -this.speedY;
   }
-
   draw() {
-    fill(this.color);
+    // fill(this.color);
+    let c = img_color_xy(this.x, this.y);
+    fill(c);
     circle(this.x, this.y, this.radius);
   }
+}
+
+// Get image color corresponding to x,y on canvas
+function img_color_xy(cx, cy) {
+  // map from canvas to image coordinates - fill
+  let x = (cx / width) * img.width;
+  let y = (cy / height) * img.height;
+
+  let c = img.get(x, y);
+  return c;
 }
