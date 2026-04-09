7f9b7a8d1fcd5325b23a2a9ef6d6d271d5280f68
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2026-04-09 17:22:40 -0400

    Particle.js

---
+++ ims04/media-particle/Particle.js
@@ -0,0 +1,33 @@
+class Particle {
+  constructor(x, y, c) {
+    this.x = x;
+    this.y = y;
+    this.c = c;
+    this.homeX = x;
+    this.homeY = y;
+  }
+  selectColor() {
+    this.c = img_color_xy(this.x, this.y);
+  }
+  update() {
+    // mouse
+    let ballDistance = dist(this.x, this.y, ball.x, ball.y);
+    let ballAngle = atan2(this.y - ball.y, this.x - ball.x);
+    // home
+    let homeDistance = dist(this.x, this.y, this.homeX, this.homeY);
+    let homeAngle = atan2(this.homeY - this.y, this.homeX - this.x);
+    // forces
+    let ballRepulsion = constrain(map(ballDistance, 0, 100, 10, 0), 0, 10);
+    let homeAttraction = map(homeDistance, 0, 100, 0, 10);
+    let vx = cos(ballAngle) * ballRepulsion;
+    vx += cos(homeAngle) * homeAttraction;
+    let vy = sin(ballAngle) * ballRepulsion;
+    vy += sin(homeAngle) * homeAttraction;
+    this.x += vx;
+    this.y += vy;
+  }
+  draw() {
+    fill(this.c);
+    ellipse(this.x, this.y + ayoffset, res, res);
+  }
+}
--- ims04/media-particle/sketch.js
+++ ims04/media-particle/sketch.js
@@ -109,40 +109,6 @@ function refreshParticles() {
   }
 }
 
-class Particle {
-  constructor(x, y, c) {
-    this.x = x;
-    this.y = y;
-    this.c = c;
-    this.homeX = x;
-    this.homeY = y;
-  }
-  selectColor() {
-    this.c = img_color_xy(this.x, this.y);
-  }
-  update() {
-    // mouse
-    let ballDistance = dist(this.x, this.y, ball.x, ball.y);
-    let ballAngle = atan2(this.y - ball.y, this.x - ball.x);
-    // home
-    let homeDistance = dist(this.x, this.y, this.homeX, this.homeY);
-    let homeAngle = atan2(this.homeY - this.y, this.homeX - this.x);
-    // forces
-    let ballRepulsion = constrain(map(ballDistance, 0, 100, 10, 0), 0, 10);
-    let homeAttraction = map(homeDistance, 0, 100, 0, 10);
-    let vx = cos(ballAngle) * ballRepulsion;
-    vx += cos(homeAngle) * homeAttraction;
-    let vy = sin(ballAngle) * ballRepulsion;
-    vy += sin(homeAngle) * homeAttraction;
-    this.x += vx;
-    this.y += vy;
-  }
-  draw() {
-    fill(this.c);
-    ellipse(this.x, this.y + ayoffset, res, res);
-  }
-}
-
 class Ball {
   constructor(x_, y_, name_) {
     this.x = x_ || random(width);
